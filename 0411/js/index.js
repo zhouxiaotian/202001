f0bbdd8abb957f124bac6c35420e5f1c6b4526e3
zhouxiaotian <1282347298@qq.com>
2020-04-11 17:48:21 +0800

    图片延迟加载

--- 0411/js/index.js
+++ 0411/js/index.js
@@ -39,7 +39,7 @@ let cascadeFlowModule = (function () {
 			columns.sort((a, b) => {
 				return b.offsetHeight - a.offsetHeight;
 			});
-			
+
 			// 循环三个数据中的每一项：每循环一项，创建一个CARD，把创建的CARD放到对应的列中即可
 			group.forEach((item, index) => {
 				let {
@@ -48,7 +48,17 @@ let cascadeFlowModule = (function () {
 					title,
 					height
 				} = item;
-				let str = `<div class="card">
+				let card = document.createElement('div');
+				card.className = "card";
+				card.innerHTML = `<a href="${link}">
+					<div class="lazyImageBox" style="height:${height}px">
+						<img src="" alt="" data-image="${pic}">
+					</div>
+					<p>${title}</p>
+				</a>`;
+				columns[index].appendChild(card);
+				
+				/* let str = `<div class="card">
 					<a href="${link}">
 						<div class="lazyImageBox" style="height:${height}px">
 							<img src="" alt="" data-image="${pic}">
@@ -56,18 +66,76 @@ let cascadeFlowModule = (function () {
 						<p>${title}</p>
 					</a>
 				</div>`;
-
-				columns[index].innerHTML += str;
-				//=>原来添加的不能删掉，所以是加等于
+				columns[index].innerHTML += str;//=>原来添加的不能删掉，所以是加等于 */
 			});
 		}
 	};
 
+	// 延迟加载
+	let lazyFunc = function lazyFunc() {
+		// 所有的lazyImageBoxs都要进行延迟加载的处理
+		let lazyImageBoxs = document.querySelectorAll('.lazyImageBox');
+		[].forEach.call(lazyImageBoxs, lazyImageBox => {
+			// 处理过的盒子就不要在处理了
+			let isLoad = lazyImageBox.getAttribute('isLoad');
+			if (isLoad === "true") return;
+			// 获取lazyImageBox一半的位置距离BODY顶端的距离
+			// 获取浏览器底边距离BODY顶端的距离
+			let B = utils.offset(lazyImageBox).top +
+				lazyImageBox.offsetHeight / 2;
+			let A = document.documentElement.clientHeight +
+				document.documentElement.scrollTop;
+			// 延迟加载的条件：盒子距离BODY的距离小于浏览器距离BODY的距离
+			if (B <= A) {
+				// 单张加载处理
+				lazyImg(lazyImageBox);
+			}
+		});
+	};
+	let lazyImg = function lazyImg(lazyImageBox) {
+		let img = lazyImageBox.querySelector('img'),
+			dataImage = img.getAttribute('data-image'),
+			tempImage = new Image;
+		tempImage.src = dataImage;
+		tempImage.onload = () => {
+			img.src = dataImage;
+			utils.css(img, 'opacity', 1);
+		};
+		img.removeAttribute('data-image');
+		tempImage = null;
+		// 给当前处理的盒子记录已经处理过的标识  setAttribute设置的自定义属性值都是字符串（即使编写的不是字符串，最后转换为字符串）
+		lazyImageBox.setAttribute('isLoad', 'true');
+	};
+
+	// 加载更多数据
+	let isRender;
+	let loadMoreData = function loadMoreData() {
+		let HTML = document.documentElement;
+		// 一屏幕的高度+卷去的高度+半屏幕高度 >= 真实高度  
+		// 滚动到底部了
+		if (HTML.clientHeight + HTML.clientHeight / 2 + HTML.scrollTop >= HTML.scrollHeight) {
+			// 加载更多数据：获取数据（此时我们获取一样的，真实项目中我们可以传递不同的参数给服务器，获取更多不同的数据）+继续绑定数据+新绑定的数据做一下延迟加载
+			// isRender：每一次重新加载之前，标记为true证明正在加载中，后期条件再次出发，只要没有加载完，不会重新加载；在第一次加载完，在让其变为false...
+			if (isRender) return;
+			isRender = true;
+			queryData();
+			bindHTML();
+			lazyFunc();
+			isRender = false;
+		}
+	};
 
 	return {
 		init() {
 			queryData();
 			bindHTML();
+			lazyFunc();
+			window.onscroll = function () {
+				// 现有图片的延迟加载
+				lazyFunc();
+				// 加载更多数据
+				loadMoreData();
+			};
 		}
 	}
 })();
