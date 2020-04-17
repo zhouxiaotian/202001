2037d435116f3c33114f2a6972e12da90358489b
zhouxiaotian <1282347298@qq.com>
2020-04-17 10:54:05 +0800

    复习瀑布流

---
+++ 0417/瀑布流/js/index.js
@@ -0,0 +1,102 @@
+let imageModule = (function () {
+	// 获取需要操作的元素
+	let $columns = $('.column'),
+		$lazyBoxs = null,
+		_data = [];
+
+	// 从服务器获取数据（AJAX请求）
+	let queryData = function queryData() {
+		// JQ中提供了从服务获取数据的AJAX方法
+		$.ajax({
+			// 请求的接口地址
+			url: 'json/data.json',
+			// 请求方式
+			method: 'get',
+			// 把从服务器获取的结果预设为JSON对象格式
+			dataType: 'json',
+			// 开始同步请求（在数据没有请求回来之前，其余的事情都无法处理）
+			async: false,
+			// JQ封装：当从服务器获取到数据后，触发的函数，形参RESULT存储的值就是我们从服务器端获取的数据
+			success: result => {
+				_data = result;
+			}
+		});
+	};
+
+	// 实现数据的绑定
+	let bindHTML = function bindHTML() {
+		// 计算图片渲染的宽度和高度
+		_data = _data.map(item => {
+			let {
+				width,
+				height
+			} = item;
+			// 计算宽度缩放比例
+			let n = width / 230;
+			// 获取到渲染的高度
+			height = height / n;
+			// 进行替换
+			item.width = 230;
+			item.height = height;
+			return item;
+
+			/* return {
+				// 把之前的数据拷贝一份
+				...item,
+				// 在把宽高修改一下即可
+				width: 230,
+				height: height
+			}; */
+		});
+
+		// 循环获取的数据（每三个为一组进行循环）
+		for (let i = 0; i < _data.length; i += 3) {
+			// GROUP获取的每一组数据
+			let group = _data.slice(i, i + 3);
+
+			// 第一次不排序直接插入
+			if (i !== 0) {
+				// 把数据升序，把列降序
+				// 值得注意的是：从服务器获取的图片，它们的宽度不一定是一样的，也不一定和最后渲染到页面中的宽度是一致的，所以我们需要按照最后的渲染宽度，计算出图片的缩放比例，从而得到最后准确渲染的高度，以这个高度进行排序才是准确的
+				group.sort((a, b) => a.height - b.height);
+				// $.fn.sort===Array.prototype.sort
+				$columns.sort((a, b) => b.offsetHeight - a.offsetHeight);
+			}
+
+			// 把排好序的数据插入到每一列中（循环GROUP中的每一项，根据每一项的数据动态创建CARD盒子，最后插入到指定的列中即可） =>基于JQ中的EACH遍历
+			$.each(group, (index, item) => {
+				let {
+					link,
+					pic,
+					height,
+					title
+				} = item;
+				$(`<div class="card">
+					<a href="${link}">
+						<div class="lazyImageBox" 
+							style="height:${height}px">
+							<img src="" alt="" data-image="${pic}">
+						</div>
+						<p>${title}</p>
+					</a>
+				</div>`).appendTo($columns.eq(index));
+			});
+		}
+
+		// 每一次数据绑定完成，我们最好获取到所有的lazyImageBox
+	};
+
+	// 图片的延迟加载（出现在视口中的图片延迟加载）
+	let lazyFunc = function lazyFunc() {};
+
+	// 加载更多的数据
+
+	return {
+		init() {
+			queryData();
+			bindHTML();
+			lazyFunc();
+		}
+	};
+})();
+imageModule.init();
