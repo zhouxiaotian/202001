6c468bc0e4378a7718c42afc32deb410df3c5bdd
zhouxiaotian <1282347298@qq.com>
2020-04-25 12:24:01 +0800

    轮播图

--- 0425/js/banner-plugin.js
+++ 0425/js/banner-plugin.js
@@ -28,7 +28,9 @@
 	}
 
 	function _each(obj, callback, context = window) {
-		obj = _cloneDeep(obj);
+		if (/^(ARRAY|OBJECT)$/i.test(obj.constructor)) {
+			obj = _cloneDeep(obj);
+		}
 		if (isArrayLike(obj)) {
 			for (let i = 0; i < obj.length; i++) {
 				let res = callback && callback.call(context, obj[i], i);
@@ -88,21 +90,88 @@
 				this[key] = item;
 			});
 			this.activeIndex = this.initialSlide;
+			this.init();
+		}
+		// BANNER.PROTOTYPE  实例.XXX()
+		init() {
+			// 入口，在这里控制代码执行的逻辑顺序
+			this.computed();
+
+		}
+		computed() {
+			// 先获取需要操作的元素
+			let container = this.container;
+			this.wrapper = container.querySelector('.zhufeng-wrapper');
+			this.slidesTrue = container.querySelectorAll('.zhufeng-slide');
+			// 克隆第一张到容器的末尾
+			this.wrapper.appendChild(this.slidesTrue[0].cloneNode(true));
+			this.slides = container.querySelectorAll('.zhufeng-slide');
+
+			// 分页器
+			this.paginationBox = null;
+			this.paginationList = null;
+			if (toType(this.pagination) === "object") {
+				let el = this.pagination.el;
+				if (el) {
+					this.paginationBox = container.querySelector(el);
+					// 创建SPAN
+					let str = ``;
+					_each(this.slidesTrue, item => {
+						str += `<span></span>`;
+					});
+					this.paginationBox.innerHTML = str;
+					this.paginationList = this.paginationBox.querySelectorAll('span');
+				}
+			}
+
+			// 左右切换
+			this.arrowPrev = null;
+			this.arrowNext = null;
+			if (toType(this.navigation) === "object") {
+				this.navigation.prevEl ? this.arrowPrev = container.querySelector(this.navigation.prevEl) : null;
+				this.navigation.nextEl ? this.arrowNext = container.querySelector(this.navigation.nextEl) : null;
+			}
+
+			// 控制元素的样式（包含初始展示哪一个）
+			this.changeWidth = parseFloat(window.getComputedStyle(this.container).width);
+			this.activeIndex = this.activeIndex < 0 ? 0 : (this.activeIndex > this.slides.length - 1 ? this.slides.length - 1 : this.activeIndex);
+			this.wrapper.style.width = `${this.changeWidth*this.slides.length}px`;
+			this.wrapper.style.transition = `left ${this.speed}ms`;
+			this.wrapper.style.left = `${-this.activeIndex*this.changeWidth}px`;
+			_each(this.slides, item => {
+				item.style.width = `${this.changeWidth}px`;
+			});
+			
+			// 默认焦点对齐
+			this.autoFocus();
+		}
+		autoFocus() {
+			// 实现焦点对齐
+			if (!this.paginationList) return;
+			let temp = this.activeIndex;
+			temp === this.slides.length - 1 ? temp = 0 : null;
+			_each(this.paginationList, (item, index) => {
+				if (index === temp) {
+					item.className = 'active';
+					return;
+				}
+				item.className = '';
+			});
 		}
 	}
-	
+
 	function bannerPlugin(container, options = {}) {
 		let defaultParams = {
 			initialSlide: 0,
 			autoplay: 3000,
 			speed: 300,
 			pagination: {
-				el: '.pagination',
+				el: '.zhufeng-pagination',
 				clickable: true
 			},
 			navigation: {
-				nextEl: '.changeRight',
-				prevEl: '.changeLeft'
+				nextEl: '.zhufeng-arrow-next',
+				prevEl: '.zhufeng-arrow-prev'
 			},
 			on: {
 				init() {},
