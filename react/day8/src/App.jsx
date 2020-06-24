3b0fa7452578d5808cc5349450ad34c9e2db2b4d
zhouxiaotian <1282347298@qq.com>
2020-06-24 12:10:06 +0800

    react

--- react/day8/src/App.jsx
+++ react/day8/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Task from './views/Task';
+import Test from './components/Test';
 
 export default class App extends React.Component {
 	render() {
 		return <>
-			<Task></Task>
+			{/* <Task></Task> */}
+			<Test />
 		</>;
 	}
 };
