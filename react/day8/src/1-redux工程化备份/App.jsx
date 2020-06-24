96cd7ccd89647c1fe9f643eb3ac17c2e80dc3613
zhouxiaotian <1282347298@qq.com>
2020-06-24 15:42:11 +0800

    react

---
+++ react/day8/src/1-redux工程化备份/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Task from './views/Task';
+import Test from './components/Test';
+
+export default class App extends React.Component {
+	render() {
+		return <>
+			{/* <Task></Task> */}
+			<Test />
+		</>;
+	}
+};
